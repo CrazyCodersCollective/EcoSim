6cbcfadaf15f31de396403083b70d08494695de3
Nico Schumann <nico.schumann@startmail.com>
2018-09-20 21:49:56 +0200

    Ugly unix hack.

--- EcoSimGame/Source/main.cpp
+++ EcoSimGame/Source/main.cpp
@@ -2,8 +2,26 @@
 #include "GameApp.h"
 #include "Game.h"
 
+#if defined(unix) || defined(__unix) || defined(__unix__)
+#define x
+#include <unistd.h>
+#include <string>
+#endif
+
 int main(int argc, char* argv[])
 {
+	// Ugly hack to ensure that the program runs in its
+	// correct current working directory (cwd).
+	// Necessary to find the location of the media files.
+#ifdef x
+    std::string cwd = argv[0];  // the invocation path
+    int count = cwd.size() - 1;
+    for( auto it = cwd.end()-1;  *it != '/' && it != cwd.begin();  --it,--count )
+    {};
+    cwd = cwd.substr( 0, count);
+    chdir( cwd.c_str());  // set the working directory
+ #endif
+
 	//GameApp* gameApp = new GameApp();
 	//gameApp->Run();
 	//delete gameApp;
